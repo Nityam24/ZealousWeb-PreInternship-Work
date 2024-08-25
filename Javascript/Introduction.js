d31ea3a2f2948a546d50d7bfb0f4c35161cb408e
Nityam24 <nityampatel2402@gmail.com>
2024-08-25 16:12:17 +0530

    Added:data types

---
+++ Javascript/Introduction.js
@@ -0,0 +1,46 @@
+//--------------------------------------Adding JavaScript to a Web Page----------------------------------------------------
+
+// Inline Script
+// Create a project folder on your desktop or in any location, name it 30DaysOfJS and create an index.html file in the project folder. Then paste the following code and open it in a browser, for example Chrome.
+
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <title>30DaysOfScript:Inline Script</title>
+//   </head>
+//   <body>
+//     <button onclick="alert('Welcome to 30DaysOfJavaScript!')">Click Me</button>
+//   </body>
+// </html>
+
+
+// Internal Script
+// The internal script can be written in the head or the body, but it is preferred to put it on the body of the HTML document. 
+
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <title>30DaysOfScript:Internal Script</title>
+//   </head>
+//   <body>
+//     <button onclick="alert('Welcome to 30DaysOfJavaScript!');">Click Me</button>
+//     <script>
+//       console.log('Welcome to 30DaysOfJavaScript')
+//     </script>
+//   </body>
+// </html>
+
+
+// External Script
+
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <title>30DaysOfJavaScript:External script</title>
+//     <script src="introduction.js"></script>
+//   </head>
+//   <body></body>
+// </html>
+
+
+
