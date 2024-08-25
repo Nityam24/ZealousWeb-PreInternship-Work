e9f6bf0155148806eb3d820060d279ce83b5bfb7
Nityam24 <nityampatel2402@gmail.com>
2024-08-25 16:46:45 +0530

    Added:Loop statements

---
+++ Javascript/Loop_statement.js
@@ -0,0 +1,91 @@
+//--------------------------------------------Loop Statements-------------------------------------------------------------------
+
+// 1. for loop
+
+// Syntax
+// for(initialization; condition; increment/decrement){
+//     // code block
+// }
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+// 2. while loop
+
+// Syntax
+// initialization
+// while(condition){
+//     // code block
+//     increment/decrement
+// }
+
+// let i = 0
+// while(i < 10){
+//     console.log(i)
+//     i++
+// }
+
+// 3. do-while loop
+
+// Syntax
+// initialization
+// do{
+//     // code block
+//     increment/decrement
+// }while(condition)
+
+// let i = 0
+// do{
+//     console.log(i)
+//     i++
+// }while(i < 10)
+
+// 4. for of loop
+
+// Syntax
+// for(variable of iterable){
+//     // code block
+// }
+
+// let lang = 'JavaScript'
+// let stack = ['HTML', 'CSS', 'JavaScript', 'React']
+// for(let char of lang){
+//     console.log(char)
+// }
+
+// for(let item of stack){
+//     console.log(item)
+// }
+
+// 5. for in loop
+
+// Syntax
+// for(variable in object){
+//     // code block
+// }
+
+// let user = {
+//     name: 'Asabeneh',
+//     role: 'teaching',
+//     country: 'Finland'
+// }
+// for(let key in user){
+//     console.log(key, user[key])
+// }
+
+// 6. forEach loop
+
+// Syntax
+// array.forEach(function(item, index, array){
+//     // code block
+// })
+
+// let countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+// countries.forEach(function(country, index, array){
+//     console.log(index, country.toUpperCase())
+// })
+
+
+
+
